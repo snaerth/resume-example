f584c90c4f6da1c40f4c7ecdab5348eb51362d2f
Snær Seljan Þóroddsson <snaerth@gmail.com>
2017-04-07 16:35:05 +0000

    Started implement resume container

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -7,20 +7,36 @@ import * as actionCreators from '../../common/actions';
 import './resume.css';
 
 class Resume extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { tl: new TimelineLite() };
+    }
+
+    componentDidMound() {
+        const container = this.refs.container;
+        const rows = container.children;
+        // TOTO implement animation rows
+    }
+
     render() {
         return (
-            <div className="resume-row">
-                <div className="resume-left">
-                    <h2>Starfsferill</h2>
-                    <p>Ferill minn bla bla bla</p>
+            <div className="resume-container" ref="container">
+                <div className="resume-row">
+                    <div className="resume-left">
+                        <h2>Starfsferill</h2>
+                    </div>
+                    <div className="resume-right">
+                        <p>Vefforritun hjá Advania síðastliðin 2 ár.Þar hef ég sinnt störfum vefforritara og hef komið aðgerð, hönnun og þróun margra vefsíðna, kerfa og verkefna.Ég hef reynslu bæði í fram-og bakendaforritun.Einnig hef ég reynslu af verktakavinnuhjá ýmsum fyrirtækjum og má þar nefna Símann,Íslandsbanka og Borgun.Kunnáttamínog helstu verkefni má sjá nánar í kaflanumtölvukunnátta</p>
+                    </div>
                 </div>
-                <div className="resume-right">
-                    <p>Vefforritun hjá Advania síðastliðin 2 ár.Þar hef ég sinnt störfum vefforritara og hef komið aðgerð, hönnun og þróun margra vefsíðna, kerfa og verkefna.Ég hef reynslu bæði í fram-og bakendaforritun.Einnig hef ég reynslu af verktakavinnuhjá ýmsum fyrirtækjum og má þar nefna Símann,Íslandsbanka og Borgun.Kunnáttamínog helstu verkefni má sjá nánar í kaflanumtölvukunnátta</p>
+                <div className="resume-row">
+                    <div className="resume-left">
+                        <h2>Starfsferill</h2>
+                    </div>
+                    <div className="resume-right">
+                        <p>Vefforritun hjá Advania síðastliðin 2 ár.Þar hef ég sinnt störfum vefforritara og hef komið aðgerð, hönnun og þróun margra vefsíðna, kerfa og verkefna.Ég hef reynslu bæði í fram-og bakendaforritun.Einnig hef ég reynslu af verktakavinnuhjá ýmsum fyrirtækjum og má þar nefna Símann,Íslandsbanka og Borgun.Kunnáttamínog helstu verkefni má sjá nánar í kaflanumtölvukunnátta</p>
+                    </div>
                 </div>
-                <a
-                    href="mailto:snaerth@gmail.com"
-                    title="Senda póst snaerth@gmail.com"
-                    className="link-slideright">snaerth@gmail.com</a>
             </div>
         );
     }
