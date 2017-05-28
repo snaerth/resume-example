e4b1c3e36f215d3c4d79b00db8fec53bb7e5df9c
Snaer Seljan Thoroddsson <snaerth@gmail.com>
2017-05-28 15:15:25 +0000

    minor ie fixes

--- src/components/pageSlideEffect/pageSlideEffect.js
+++ src/components/pageSlideEffect/pageSlideEffect.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './pageSlideEffect.css';
 
 class PageSlideEffect extends Component {
   render() {
-    const {type} = this.props;
+    const { type } = this.props;
     return (
       <div
         className={`revealer revealer--animate anim--effect-${type} revealer--${type}`}
@@ -17,7 +17,7 @@ class PageSlideEffect extends Component {
 }
 
 PageSlideEffect.propTypes = {
-  type: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
 };
 
 export default PageSlideEffect;
