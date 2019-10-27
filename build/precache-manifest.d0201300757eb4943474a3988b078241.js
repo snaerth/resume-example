0e88310c98236f8b1409c631b20e8bbe9841baf1
Snær Seljan Þóroddsson <snaer@dohop.com>
2019-10-27 21:45:28 +0000

    Updated build

---
+++ build/precache-manifest.d0201300757eb4943474a3988b078241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6beaf9c80621a274f2ea730c4e69a5fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b439052f2e422a869f",
+    "url": "/static/css/main.a5c85c27.chunk.css"
+  },
+  {
+    "revision": "0ad17fe930747b59f540",
+    "url": "/static/js/2.915cb9b4.chunk.js"
+  },
+  {
+    "revision": "09b439052f2e422a869f",
+    "url": "/static/js/main.c1901c1a.chunk.js"
+  },
+  {
+    "revision": "cc5ab63f0c1795ef03b6",
+    "url": "/static/js/runtime-main.58aedfe6.js"
+  },
+  {
+    "revision": "d4b0ca1fdc9f868b395a8271d8dfb8b4",
+    "url": "/static/media/snaer_seljan_thoroddsson.d4b0ca1f.png"
+  }
+]);
